c3798f07d90d1a2f66532c936bb301b7b4c643b9
Quizznor <paulfilip96@yahoo.de>
2020-09-08 11:26:37 +0000

    Work on polybar

--- scripts/polybar-launch.sh
+++ scripts/polybar-launch.sh
@@ -3,4 +3,4 @@
 # Called by i3 config reload, restarts all polybars with the fitting config
 
 killall -q polybar
-polybar --config=/home/quizznor/.config/polybar/config main
+polybar --config=/home/quizznor/.config/polybar/config.ini main
