412fdce921eacfa1e6718d8af599272e13c6350e
Quizznor <paulfilip96@yahoo.de>
2020-09-09 11:51:42 +0000

    Beautify polybar

--- scripts/polybar-launch.sh
+++ scripts/polybar-launch.sh
@@ -2,5 +2,19 @@
 
 # Called by i3 config reload, restarts all polybars with the fitting config
 
-killall -q polybar
-polybar --config=/home/quizznor/.config/polybar/config.ini main
+killall -q polybar cbatticon nm-applet volumeicon
+polybar --config=/home/quizznor/.config/polybar/config.ini main &
+
+# also repopulate sys-tray with important status icons
+# Simply add the application of your choice in TRAYVARS
+
+TRAYVARS=("cbatticon" "nm-applet" "volumeicon")
+
+for VAR in "${TRAYVARS[@]}"
+do
+  while [ "$(pidof $VAR)" == "" ]
+  do
+    $VAR &
+  done
+  sleep 0.1
+done
---
+++ scripts/rofi-calendar.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DATEFTM="${DATEFTM:-+%a. %d. %b. %Y}"
+SHORTFMT="${SHORTFMT:-+%d.%m.%Y}"
+LABEL="${LABEL}"
+blockdate=$(date "$DATEFTM")
+shortblockdate=$(date "$SHORTFMT")
+
+year=$(date '+%Y')
+month=$(date '+%m')
+date=$(date '+%A, %d. %B')
+
+export TERM=xterm
+cal --color=always $month $year \
+    | sed 's/\x1b\[[7;]*m/\<b\>\<u\>/g' \
+    | sed 's/\x1b\[[27;]*m/\<\/u\>\<\/b\>/g' \
+    | tail -n +2 \
+    | rofi \
+        -dmenu \
+        -markup-rows \
+        -no-fullscreen \
+        -font "Monospace 13" \
+        -theme Poly-Calender \
+        -hide-scrollbar \
+        -bw 2 \
+        -lines 6 \
+        -m -3 \
+        -theme-str '#window {anchor:south;location:west;x-offset:-280px;y-offset:-160px;width:16.5%;}' \
+        -eh 1 \
+        -width -22 \
+        -no-custom \
+        -p "$blockdate" > /dev/null
