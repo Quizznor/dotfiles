ff115b58f9919228fdcc02f161d330baa2b3e49a
Quizznor <paulfilip96@yahoo.de>
2020-09-14 22:46:20 +0000

    Upload powermenu

--- scripts/rofi-powermenu.sh
+++ scripts/rofi-powermenu.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+OPTIONS=("  Power off" "\n  Standby" "\n  Restart" "\n  Logout")
+
 case $1 in
   "")
   echo ""
   ;;
   "open")
-  rofi -dmenu -theme powermenu -p "test"
+  SELECTED=$(echo -en "${OPTIONS[@]}" | rofi -dmenu -theme powermenu -p " System Option" -l 4)
+
+  case $SELECTED in
+    *""*)
+    systemctl poweroff
+    ;;
+    *""*)
+    systemctl suspend
+    ;;
+    *""*)
+    systemctl reboot
+    ;;
+    *""*)
+    i3-msg exit
+    ;;
+  esac
   ;;
 esac
