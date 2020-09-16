64427b98180da3551c79236245ddf1a3df090bc3
Quizznor <paulfilip96@yahoo.de>
2020-09-16 11:44:49 +0000

    Finish work on todo utility

--- scripts/conky-todo.py
+++ scripts/conky-todo.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-A, B, C = [], [], []
-
-#for i,line in enumerate(open("/home/quizznor/Documents/todo.txt").readlines()):
-#    if("[URGENT]" in line):
-#        print(line)
-#        A+=str(line)
-
 tasks_done = len(open("/home/quizznor/Documents/todone.txt").readlines())
-tasks_left = 2
+tasks_open = open("/home/quizznor/Documents/todo.txt").readlines()
+groups = ["[URGENT]","[MEDIUM]","[RELAX ]"]
+colors = ["red", "yellow", "green"]
+tasks_left = len(tasks_open)
 
-#print(A)
+print("\n\t  TODO LIST: %i OPEN, %i COMPLETED"%(tasks_left,tasks_done))
+print("-"*48+"\n")
+
+for i,group in enumerate(groups):
+    for j,line in enumerate(tasks_open):
+        if group in line:
+            print(" ${color %s}"%(colors[i])+str(j+1).zfill(2)+" "+line.rstrip(),"$color")
+    print("\n")
+
+#for i,line in enumerate(tasks):
+#    print(line)
+#    for i,string in enumerate():
+#        if(string in line):
+#            print(str(i)+"  "+line)
+            #tasks[j].append(str(i)+"   "+line)
