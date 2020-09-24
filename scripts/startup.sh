5c0dc91568485a91df2ccf7f092d20537f0a175a
Quizznor <paulfilip96@yahoo.de>
2020-09-24 21:07:34 +0200

    Delete startup.sh

--- scripts/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-i3-msg "workspace 1; append_layout /home/quizznor/.config/i3/layout-home.json"
-kitty --config=$HOME/.config/kitty/kitty.ini &
-conky --config=$HOME/.config/conky/todo.ini &
-#i3-msg "workspace 11" > /dev/null &
-#sleep 0.5
-#i3-msg "workspace 1"
