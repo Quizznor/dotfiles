fa170f2ff65f12e2834ef792a1508d70cf9f18f8
Quizznor <paulfilip96@yahoo.de>
2020-10-05 00:18:45 +0200

    Work on ranger

--- scripts/mv-writing.sh
+++ scripts/mv-writing.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-STATUS=$(i3-msg -t get_workspaces | awk '/"num":6/ {print}')
+STATUS=$(i3-msg -t get_workspaces | awk '/"num":4/ {print}')
 
-i3-msg workspace 6 > /dev/null
+i3-msg workspace 4 > /dev/null
 
 if [ "$STATUS" == "" ]
 then
-  i3-msg "workspace 6; append_layout $HOME/.config/i3/layout-writing.json"
-  kitty --config=$HOME/.config/kitty/kitty.ini --directory=$HOME/writing/ &
+  i3-msg "workspace 4; append_layout $HOME/.config/i3/layout-writing.json"
+  kitty --config=$HOME/.config/kitty/kitty.ini --directory=$HOME/writing/ ranger &
   conky --config=$HOME/.config/conky/shortcuts.ini &
 fi
