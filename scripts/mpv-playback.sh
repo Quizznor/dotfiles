2d0eae9fa73e2323f04de1df092164ab17014e68
Quizznor <paul.filip@protonmail.com>
2021-03-16 16:08:58 +0100

    Improvements on music desktop

--- scripts/mpv-playback.sh
+++ scripts/mpv-playback.sh
@@ -3,6 +3,7 @@
 if [ -z $(pidof mpv) ]; then
   # start mpv with desired parameters
   mpv --quiet --force-window=yes --keep-open=yes --input-ipc-server=/tmp/mpv-socket $@ > /dev/null & disown
+  sleep 0.3 && i3-msg "[title=RANGER] focus"
 else
   # append to playlist if its already running
   echo "loadfile $@ append-play" | socat - /tmp/mpv-socket &
--- scripts/mv-music.sh
+++ scripts/mv-music.sh
@@ -7,6 +7,8 @@ i3-msg workspace 5 > /dev/null
 if [ "$STATUS" == "" ]
 then
   i3-msg "workspace 5; append_layout $HOME/.config/i3/layout-music.json"
-  kitty --title="RANGER" --directory=$HOME/media ranger > /dev/null & disown
+  kitty --title="RANGER" --directory=$HOME/music/artists ranger > /dev/null & disown
   kitty --title="VIS" --config=$HOME/.config/kitty/kitty-vis.ini vis > /dev/null & disown
 fi
+
+sleep 0.4 && i3-msg "[title=RANGER] focus"
