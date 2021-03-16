f1ccc570a08489be7098bfb8e7d6280942ec44e7
Quizznor <paul.filip@protonmail.com>
2021-03-16 16:37:28 +0100

    Migrate git repositories from /home/quizznor

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-mv "$@" $HOME/.trash/
+# mv would result in the age of the file not being updated,
+# which in some cases means immediate deletion, very sad =(
+cp -r "$@" $HOME/.trash/
+rm -rf "$@"
+
 find $HOME/.trash/* -mtime +7 -exec rm -rf {} \;
--- scripts/mv-music.sh
+++ scripts/mv-music.sh
@@ -7,7 +7,7 @@ i3-msg workspace 5 > /dev/null
 if [ "$STATUS" == "" ]
 then
   i3-msg "workspace 5; append_layout $HOME/.config/i3/layout-music.json"
-  kitty --title="RANGER" --directory=$HOME/music/artists ranger > /dev/null & disown
+  kitty --title="RANGER" --directory=$HOME/git-repositories/music/artists ranger > /dev/null & disown
   kitty --title="VIS" --config=$HOME/.config/kitty/kitty-vis.ini vis > /dev/null & disown
 fi
 
