425cd46c45707f0541a3ace1413c1ab15e3fa57d
Quizznor <paul.filip@protonmail.com>
2021-11-13 23:58:27 +0100

    Fix audiosink issues

--- scripts/rofi-audiosink.sh
+++ scripts/rofi-audiosink.sh
@@ -2,10 +2,10 @@
 
 # use pavucontrol to change the codec of the headset, if desired
 
-PROMPT=("  \n  \n  \n  ")
+PROMPT=("  \n  \n  \n  \n  ")
 HEADPHONE_ADDRESS="98:52:3D:27:D1:02"
 SPEAKER_SINK="alsa_output.pci-0000_00_1f.3.analog-stereo"
-HEADPHONE_SINK="alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"
+HEADPHONE_SINK="alsa_output.usb-GeneralPlus_USB_Audio_Device-00.iec958-stereo"
 BLUETOOTH_SINK="bluez_sink.98_52_3D_27_D1_02.a2dp_sink"
 HDMI_SINK="alsa_output.pci-0000_00_1f.3.hdmi-stereo"
 
@@ -16,10 +16,14 @@ case $SELECTED in
   *""*) # Notebook speakers
   bluetoothctl power off
   pacmd set-default-sink $SPEAKER_SINK    # Whichever of these is currently connected
-  pacmd set-default-sink $HEADPHONE_SINK  # prefer headphone over default speakers
   ;;
 
-  *""*) # Bluetooth headset
+  *""*) # Headphone with cable
+  bluetoothctl power off
+  pacmd set-default-sink $HEADPHONE_SINK
+  ;;
+  
+  *""*) # Bluetooth headset
   RUNTIME=$((SECONDS+10)) # Attempt to connect to the headset. Timeout after 10 seconds (~9-10 retries)
   while [[ -z "$(pacmd list-sinks | awk '/bluez/{print}')" && $SECONDS -lt $RUNTIME ]]
   do
