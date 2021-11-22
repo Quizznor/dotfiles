2958bc31c1f97412e06e7ae729a60f82c66c9690
Quizznor <paul.filip@protonmail.com>
2021-11-22 23:22:49 +0100

    Add battery monitor + update safecheck

--- scripts/battery-monitor.sh
+++ scripts/battery-monitor.sh
@@ -5,8 +5,9 @@ if [ -r "$HOME/.dbus/Xdbus" ]; then
 fi
 
 BAT_LVL=$( cat /sys/class/power_supply/BAT0/capacity )
+BAT_STAT=$( cat /sys/class/power_supply/BAT0/status)
 
-if [ $BAT_LVL -lt 10 ]
+if [[ $BAT_LVL -lt 20 && $BAT_STAT != "Charging" ]]
 then
   dunstify --urgency=critical --replace 15983 " Battery low: $BAT_LVL% remaining"
 fi
--- scripts/rofi-powermenu.sh
+++ scripts/rofi-powermenu.sh
@@ -3,17 +3,28 @@
 OPTIONS=("" "\n" "\n" "\n" "\n")
 SELECTED=$(echo -en "${OPTIONS[@]}" | rofi -dmenu -theme powermenu -p " System Option" -l 5)
 
+function check_abort_action {
+  if [ -f "/var/lib/pacman/db.lck" ] then
+    dunstify --urgency=critical --replace 40195 "Update in progress!"
+    exit 1
+  fi
+}
+
 case $SELECTED in
   *""*)
+  check_abort_action
   systemctl poweroff
   ;;
   *""*)
+  check_abort_action
   systemctl suspend
   ;;
   *""*)
+  check_abort_action
   systemctl reboot
   ;;
   *""*)
+  check_abort_action
   i3-msg exit
   ;;
   *""*)
