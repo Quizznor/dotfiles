cfc7f49318fb1faef152521507811cd88e89dd64
Paul Filip <paul.filip@protonmail.com>
2023-06-13 11:30:59 +0200

    Add ranger, alacritty config

---
+++ .bashrc
@@ -0,0 +1,17 @@
+#
+# ~/.bashrc
+#
+
+# USER CHANGES  ################################################
+
+GPG_TTY="\`tty\`"
+EDITOR=/usr/bin/nano
+
+################################################################
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
--- scripts/cron-battery.sh
+++ scripts/cron-battery.sh
@@ -5,6 +5,7 @@ CHARGE_NOW=$(cat /sys/class/power_supply/BAT0/charge_now)
 CHARGE_FULL=$(cat /sys/class/power_supply/BAT0/charge_full)
 PERCENTAGE=$(( 100 * $CHARGE_NOW / $CHARGE_FULL ))
 
-if [[ $PERCENTAGE -lt 30 && "$STATUS" = "Discharging" ]]; then
+if [[ $PERCENTAGE -lt 15 && "$STATUS" = "Discharging" ]]; then
+    aplay -N $HOME/system/sounds/warning.wav &
     notify-send -a "Battery" -u "critical" "$PERCENTAGE% battery remaining!" "Run to the next power plug"
 fi
