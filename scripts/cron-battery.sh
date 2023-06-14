624e6b290e23135d7158dfb7f2cb926de117feca
Paul Filip <paul.filip@protonmail.com>
2023-06-14 12:06:57 +0200

    Streamline notifications, battery alarm

--- scripts/cron-battery.sh
+++ scripts/cron-battery.sh
@@ -5,7 +5,10 @@ CHARGE_NOW=$(cat /sys/class/power_supply/BAT0/charge_now)
 CHARGE_FULL=$(cat /sys/class/power_supply/BAT0/charge_full)
 PERCENTAGE=$(( 100 * $CHARGE_NOW / $CHARGE_FULL ))
 
-if [[ $PERCENTAGE -lt 15 && "$STATUS" = "Discharging" ]]; then
-    aplay -N $HOME/system/sounds/warning.wav &
-    notify-send -a "Battery" -u "critical" "$PERCENTAGE% battery remaining!" "Run to the next power plug"
+MESSAGE="Only $PERCENTAGE% battery remaining!" 
+BODY="Run to the next power plug"
+
+if [[ $PERCENTAGE -lt 20 && "$STATUS" = "Discharging" ]]; then
+    aplay $HOME/system/sounds/warning.wav &
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -a "Battery" -u "critical" "$MESSAGE" "$BODY"
 fi
--- scripts/notifications.sh
+++ scripts/notifications.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
+DISTURB="      ENABLE NOTIFICATIONS      (restart dunst)"
+DO_NOT_DISTURB="      DO NOT DISTURB                           (kill dunst)"
+
 if [ -z "$(pidof dunst)" ]; then
-    PROMPT="      Restart dunst"
     ICON="notifications-disabled"
+    PROMPT=$DISTURB
 else
-    PROMPT="      Do not disturb\n      Show history"
+    PROMPT=$DO_NOT_DISTURB
     ICON="notifications"
 fi
 
 if [ -z "$1" ]; then
     echo "| iconName=$ICON onclick=bash bash='$HOME/.config/scripts/notifications.sh click'"
 else
+    for i in {1..5}; do xdotool key ctrl+grave; done
+
     SELECTED=$(echo -en "${PROMPT[@]}" | rofi -dmenu -theme $HOME/.config/rofi/notification-menu.rasi)
+    
+    if [ "$PROMPT" = "$DO_NOT_DISTURB" ]; then
+        dunstctl close-all
+    fi
 
     case $SELECTED in
-    "      Do not disturb")
+    $DO_NOT_DISTURB)
         killall dunst ;;
-
-    "      Show history")
-        for i in {1..20}; do xdotool key ctrl+grave; done ;;
-
-    "      Restart dunst")
+    $DISTURB)
         dunst & ;;
     esac
 
