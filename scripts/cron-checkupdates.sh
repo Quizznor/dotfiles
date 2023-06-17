0c6438c0eade7fa5de6b1497063d3e5f89a4f63f
Paul Filip <paul.filip@protonmail.com>
2023-06-17 22:44:21 +0200

    Merge update.sh + cron-checkupdates.sh

--- scripts/cron-checkupdates.sh
+++ scripts/cron-checkupdates.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-checkupdates+aur | awk '{print $1}' > $HOME/system/.updates
+get_updates () {
+    checkupdates+aur | awk '{print $1}' > $HOME/system/.updates
+}
+
+if [ -z "$1" ]; then
+
+    MODULES=$(cat $HOME/system/.updates)
+
+    case $MODULES in
+        *"linux-headers"*)
+        icon=update-high ;;
+        "")
+        icon="" ;;
+        *)
+        icon=update-none ;;
+    esac
+
+    echo "| iconName=$icon onclick=bash bash='$HOME/.config/scripts/update.sh update'"
+else
+    if [ "$1" = "update" ]; then
+        alacritty --config-file "$HOME/.config/alacritty/update.yml" -e yay -Syu
+    fi
+
+    get_updates
+fi
