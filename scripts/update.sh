136b82a4f2a7e4e3a5961e4820c1bc4430351887
Paul Filip <paul.filip@protonmail.com>
2023-06-25 18:17:19 +0200

    Improve VSCode workflow with Insiders, automatic workspace picking

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Getting package list pre update..."
 packages=$(echo $(checkupdates+aur | awk '{print $1}'))
 
 yay -Syu
-
 
 echo "Performing post process transactions..."
 if [ "$packages" == *"firefox"* ]; then
     echo "Firefox was updated... overwriting firefox.desktop file"
     sudo mv $HOME/.config/firefox/firefox.desktop /usr/share/applications/
+fi
+
+if [ "$packages" == *"code"* ]; then
+    echo "Visual Studio Code was updated... linking vscode.desktop file"
+    sudo mv $HOME/.config/code/visual-studio-code-insiders.desktop /usr/share/applications
 fi
