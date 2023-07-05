4018b58b2ae97c8d813e3d4d26ee7fdba77df345
Paul Filip <paul.filip@protonmail.com>
2023-07-06 00:17:57 +0200

    Fix syntax in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 packages=$(cat $HOME/system/.updates)
-
-yay -Syu
+echo $packages
+#yay -Syu
 
 echo "Performing post process transactions..."
-if [ "$packages" == *"firefox"* ]; then
+if [[ "$packages" == *"firefox"* ]]; then
     echo "Firefox was updated... overwriting firefox.desktop file"
     sudo cp $HOME/.config/firefox/firefox.desktop /usr/share/applications/
 fi
 
-if [ "$packages" == *"code"* ]; then
+if [[ "$packages" == *"code"* ]]; then
     echo "Visual Studio Code was updated... linking vscode.desktop file"
     sudo cp $HOME/.config/code/visual-studio-code-insiders.desktop /usr/share/applications
 fi
