04cc675107612ccf6b4037071bf09cc50a52db2f
Paul Filip <paul.filip@protonmail.com>
2023-08-07 14:32:21 +0200

    Add startup script for work

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,10 +6,23 @@ rm -rf $HOME/system/icons/activities/*
 # force KDE to use dunst
 killall plasmashell
 dunst &
-plasmashell
+plasmashell &
 
 # turn off bluetooth
 echo -e "power off" | bluetoothctl
 
 # open mail application
 betterbird &
+
+# start work screen if large monitor is connected
+if [[ ! -z "$( xrandr | grep HDMI1)" ]]; then
+    
+    xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
+    kactivities-cli --set-current-activity "$(kactivities-cli --list-activities | grep Work | awk '{print $2}')"
+
+    wmctrl -s 1
+    alacritty -T server-htop &
+    alacritty -T server-prompt &
+    code-insiders &
+    firefox &
+fi
