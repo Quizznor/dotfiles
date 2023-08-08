d761fc2c57f3851849241ed892166015ff17fed5
Paul Filip <paul.filip@protonmail.com>
2023-08-08 14:12:22 +0200

    Automatic activity setting

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -12,17 +12,19 @@ plasmashell &
 echo -e "power off" | bluetoothctl
 
 # open mail application
-betterbird &
+# betterbird &
 
 # start work screen if large monitor is connected
 if [[ ! -z "$( xrandr | grep HDMI1)" ]]; then
     
     xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
     kactivities-cli --set-current-activity "$(kactivities-cli --list-activities | grep Work | awk '{print $2}')"
-
     wmctrl -s 1
+
     alacritty -T server-htop &
     alacritty -T server-prompt &
     code-insiders &
     firefox &
+else
+    kactivities-cli --set-current-activity "$(kactivities-cli --list-activities | grep Home | awk '{print $2}')"
 fi
