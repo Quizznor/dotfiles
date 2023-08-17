082ae36e58e200c9f237711d5d97dd9c550c6365
Paul Filip <paul.filip@protonmail.com>
2023-08-17 13:53:10 +0200

    automatic work setup

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -15,14 +15,14 @@ echo -e "power off" | bluetoothctl
 # betterbird &
 
 # start work screen if large monitor is connected
-if [[ ! -z "$( xrandr | grep HDMI1)" ]]; then
+if [[ ! -z "$( xrandr | grep 'HDMI1 connected')" ]]; then
     
     xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1
     kactivities-cli --set-current-activity "$(kactivities-cli --list-activities | grep Work | awk '{print $2}')"
-    wmctrl -s 1
+    wmctrl -s 0
 
-    alacritty -T server-htop &
-    alacritty -T server-prompt &
+    alacritty -T server-prompt -e ssh -t auger 'clear; echo "$(ps -ef | grep filip | wc -l) zombie processes active"; bash -l;' &
+    alacritty -T server-htop -e ssh -t auger 'htop --user $USER' &
     code-insiders &
     firefox &
 else
