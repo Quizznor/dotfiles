e89ab1c5f758c3268089db3c3919631810576980
quizznor <paul.filip@protonmail.com>
2024-01-12 13:20:27 +0100

    Change shell + update alacritty

--- scripts/ctl_write.sh
+++ scripts/ctl_write.sh
@@ -2,4 +2,4 @@
 
 wmctrl -s 3
 
-alacritty --config-file $HOME/.config/alacritty/ranger.yml -T "writing" -e "ranger" "$HOME/latex/"
+alacritty --config-file $HOME/.config/alacritty/ranger.toml -T "writing" -e "ranger" "$HOME/latex/"
